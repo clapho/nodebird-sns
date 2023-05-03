20a81e874a6a19436d9667bf1dab263747823a07
SoohoPark <clapp301@gmail.com>
2023-05-03 10:42:21 +0900

    refactor: useSelector와 useDispatch 적용하여 리팩토링

--- front/components/LoginForm.js
+++ front/components/LoginForm.js
@@ -2,50 +2,55 @@ import React, { useCallback, useState } from 'react';
 import { Button, Form, Input } from 'antd';
 import Link from 'next/link';
 import styled from 'styled-components';
-import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 import useInput from '../hooks/useInput';
+import { loginAction } from '../reducers';
 
 const ButtonWrapper = styled.div`
-  margin-top: 10px;
+	margin-top: 10px;
 `;
 
 const FormWrapper = styled(Form)`
-  padding: 10px;
+	padding: 10px;
 `;
 
-const LoginForm = ({ setIsLoggedIn }) => {
-  const [id, onChangeId] = useInput('');
-  const [password, onChangePassword] = useInput('');
-  const onSubmitForm = useCallback(() => {
-    setIsLoggedIn(true);
-  }, [id, password]);
+const LoginForm = () => {
+	const dispatch = useDispatch();
+	const [id, onChangeId] = useInput('');
+	const [password, onChangePassword] = useInput('');
 
-  return (
-    <FormWrapper onFinish={onSubmitForm}>
-      <div>
-        <label htmlFor="user-id">아이디</label>
-        <br />
-        <Input name="user-id" value={id} onChange={onChangeId} required />
-      </div>
-      <div>
-        <label htmlFor="user-password">비밀번호</label>
-        <br />
-        <Input name="user-password" type="password" value={password} onChange={onChangePassword} required />
-      </div>
-      <ButtonWrapper>
-        <Button type="primary" htmlType="submit" loading={false}>
-          로그인
-        </Button>
-        <Link href="/signup">
-          <Button>회원가입</Button>
-        </Link>
-      </ButtonWrapper>
-    </FormWrapper>
-  );
-};
+	const onSubmitForm = useCallback(() => {
+		dispatch(loginAction({ id, password }));
+	}, [id, password]);
 
-LoginForm.propType = {
-  setIsLoggedIn: PropTypes.func.isRequired,
+	return (
+		<FormWrapper onFinish={onSubmitForm}>
+			<div>
+				<label htmlFor="user-id">아이디</label>
+				<br />
+				<Input name="user-id" value={id} onChange={onChangeId} required />
+			</div>
+			<div>
+				<label htmlFor="user-password">비밀번호</label>
+				<br />
+				<Input
+					name="user-password"
+					type="password"
+					value={password}
+					onChange={onChangePassword}
+					required
+				/>
+			</div>
+			<ButtonWrapper>
+				<Button type="primary" htmlType="submit" loading={false}>
+					로그인
+				</Button>
+				<Link href="/signup">
+					<Button>회원가입</Button>
+				</Link>
+			</ButtonWrapper>
+		</FormWrapper>
+	);
 };
 
 export default LoginForm;
