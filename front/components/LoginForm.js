4b32b60620408827c62bc8a93132321e0feb919d
SoohoPark <clapp301@gmail.com>
2023-05-05 22:53:13 +0900

    feat: redux saga 도입

--- front/components/LoginForm.js
+++ front/components/LoginForm.js
@@ -2,9 +2,9 @@ import React, { useCallback, useState } from 'react';
 import { Button, Form, Input } from 'antd';
 import Link from 'next/link';
 import styled from 'styled-components';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import useInput from '../hooks/useInput';
-import { loginAction } from '../reducers/user';
+import { loginRequestAction } from '../reducers/user';
 
 const ButtonWrapper = styled.div`
 	margin-top: 10px;
@@ -16,11 +16,12 @@ const FormWrapper = styled(Form)`
 
 const LoginForm = () => {
 	const dispatch = useDispatch();
+	const { isLoggingIn } = useSelector((state) => state.user);
 	const [id, onChangeId] = useInput('');
 	const [password, onChangePassword] = useInput('');
 
 	const onSubmitForm = useCallback(() => {
-		dispatch(loginAction({ id, password }));
+		dispatch(loginRequestAction({ id, password }));
 	}, [id, password]);
 
 	return (
@@ -42,7 +43,7 @@ const LoginForm = () => {
 				/>
 			</div>
 			<ButtonWrapper>
-				<Button type="primary" htmlType="submit" loading={false}>
+				<Button type="primary" htmlType="submit" loading={isLoggingIn}>
 					로그인
 				</Button>
 				<Link href="/signup">
